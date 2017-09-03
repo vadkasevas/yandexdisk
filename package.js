8e919cafb13d4ea33a30fcd568e44c0f17a5b57e
vadkasevas <v.tikkoyev@mail.ru>
2017-09-03 20:45:19 +0300

    Initial commit

---
+++ package.js
@@ -0,0 +1,33 @@
+Package.describe({
+    name: 'malibun23:yandexdisk',
+    version: '0.0.1',
+    summary: 'YandexDisk',
+    git: '',
+    documentation: 'README.md'
+});
+
+Npm.depends({
+    "dom-js":"0.0.9",
+    "xml2js":"0.4.19"
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
+    api.use('ecmascript');
+    api.use('underscore');
+    api.use('malibun23:stack@0.0.17');
+
+    api.addFiles(
+        [
+            'lib/yandexdisk.js'
+        ],['server']
+    );
+
+    api.export(
+        [
+            'YandexDisk'
+        ]
+        , ['server']
+    );
+});
+
